10104034e08b89b0433fbff2ae26b95a2361bd8b
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-04-11 10:01:53 +0100

    Remove gcc options

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-lex C99_Parser.l
-yacc -dy C99_Parser.y
-yacc C99_Parser.y
-gcc main.c lex.yy.c y.tab.c Declarator.c -ll -ly -o C99_Parser
+#lex C99_Parser.l
+flex C99_Parser.l
+#yacc -dy C99_Parser.y
+bison -dy C99_Parser.y
+#yacc C99_Parser.y
+bison C99_Parser.y
+gcc main.c lex.yy.c y.tab.c Declarator.c -o C99_Parser
